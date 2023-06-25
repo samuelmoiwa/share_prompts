5a0473fe327633e78894d9eaa069d1bbf18b99a8
samuelmoiwa <samuelmoiwa@gmail.com>
2023-06-25 19:57:30 +0000

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,12 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'home-bg': 'linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);',
       },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        noto: ['Noto Serif TC', 'serif'],
+      }
     },
   },
   plugins: [],
